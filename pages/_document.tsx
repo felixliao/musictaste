85c0e5e7579b487a9b91fb42a0dce4ded1216945
Xiaofei Liao <felixliaochina@gmail.com>
2022-02-11 04:00:31 +0800

    style

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Netease Music Taste</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,7 @@ export default function Home() {
   const session = useSession()
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 px-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+
       <h1 className="text-center text-2xl">Netease Music Taste Calculator</h1>
       <p className="mt-5 text-center text-sm opacity-60">
         Assess your music taste by the comment counts on Netease Music. The
