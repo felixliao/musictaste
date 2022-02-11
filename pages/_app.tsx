c109192c97eb458682e08c2c33af9a393a1a5adb
Xiaofei Liao <felixliaochina@gmail.com>
2022-02-12 03:48:03 +0800

    ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from 'next-auth/react'
 import { useEffect } from 'react'
 import { pageview } from 'lib/ga'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const router = useRouter()
@@ -25,6 +26,20 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   }, [router.events])
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
       <title>Netease Music Taste</title>
       <SessionProvider session={session}>
         <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-        `}
-        </Script>
       </Head>
       <body>
         <Main />
