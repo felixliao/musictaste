a6d3a638e52200f93e14521ce440dc6f3cf16e11
Xiaofei Liao <felixliaochina@gmail.com>
2022-02-12 03:36:01 +0800

    ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { useEffect } from 'react'
+import { pageview } from 'lib/ga'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      console.log(url)
+      pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <>
       <title>Netease Music Taste</title>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,22 +7,19 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
+        <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          strategy="afterInteractive"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
